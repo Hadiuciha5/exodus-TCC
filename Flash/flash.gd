09a6cb9294d6793272f048a596bd0d4b223728c5
Hadiuciha5 <hadiuciha01@gmail.com>
2024-03-02 12:48:43 +0700

    rererre

--- Flash/flash.gd
+++ Flash/flash.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 func _ready():
 	$AnimationPlayer.play("fade_in")	
-	await get_tree().create_timer(6).timeout
+	await get_tree().create_timer(4).timeout
 	$AnimationPlayer.play("fade_out")
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(2).timeout
 	get_tree().change_scene_to_file("res://INTRO/intro.tscn")
 	
--- play_intro/play_intro.gd
+++ play_intro/play_intro.gd
@@ -5,3 +5,5 @@ extends Node2D
 
 func _on_touch_screen_button_pressed():
 	get_tree().change_scene_to_file("res://world/world.tscn")
+
+
