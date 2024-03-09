25d4584e7fc1efb1c3e31ffbdbc950e6eac8fffb
Hadiuciha5 <hadiuciha01@gmail.com>
2024-03-09 11:09:23 +0700

    dssd

--- play_intro/play_intro.gd
+++ play_intro/play_intro.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
+@onready var overlay := %FadeOverlay
+@onready var return_button := %kembali
 
 
+func _ready():
+	overlay.on_complete_fade_out.connect(_on_fade_overlay_on_complete_fade_out)
+	return_button.pressed.connect(_on_kembali_released)
+	
+	overlay.visible = true
+	
 
-func _on_touch_screen_button_pressed():
-	get_tree().change_scene_to_file("res://world/world.tscn")
+func _on_fade_overlay_on_complete_fade_out():
+	get_tree().change_scene_to_file("res://pick_menu/pick_menu.tscn")
+
+func _on_kembali_released():
+	overlay.fade_out()
+
 
 
